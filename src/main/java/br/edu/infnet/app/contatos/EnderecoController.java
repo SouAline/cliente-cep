9c6b2aa9bc50bf9c0d1fce2c56cf54827f39ded7
Aline Souza <ds.alinesouza@gmail.com>
2021-07-31 10:03:44 -0300

    Criando classe EnderecoController

---
+++ src/main/java/br/edu/infnet/app/contatos/EnderecoController.java
@@ -0,0 +1,66 @@
+
+package br.edu.infnet.app.contatos;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "EnderecoController", urlPatterns = {"/EnderecoController"})
+public class EnderecoController extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String cep = request.getParameter("cep");
+        if (StringUtils.isNotBlanck(cep)) {
+            EnderecoService es=new EnderecoService[];
+            Endereco end= es.obterPorCep(cep);
+            request.setAttribute("endereco", end);
+        }else{
+            request.setAttribute("erro",   "CEP Invalido")
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
