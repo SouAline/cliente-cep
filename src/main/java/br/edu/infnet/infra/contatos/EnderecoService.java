381fdd15b883313bc29fefa5b61c163b268c2d9c
Aline Souza <ds.alinesouza@gmail.com>
2021-07-31 10:04:07 -0300

    Alterando classe EnderecoService

--- src/main/java/br/edu/infnet/infra/contatos/EnderecoService.java
+++ src/main/java/br/edu/infnet/infra/contatos/EnderecoService.java
@@ -1,6 +1,17 @@
 
 package br.edu.infnet.infra.contatos;
 
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+
 public class EnderecoService {
-    private final String REST URI = "https://viacep.com.br/ws";
+    private final String REST_URI = "https://viacep.com.br/ws";
+    private final Client cliente = new ClientBuilder.newClient();
+    
+    public Endereco obterPorCep(String cep) {
+        return cliente
+                .target(REST_URI)
+                .path(cep).request(MediaType.APLICATION_JSON)
+                .get(Endereco.class);
+    }
 }
